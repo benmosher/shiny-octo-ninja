2b96e26dd1cc9312b31a9f7f2b077bd3642b340a
Ben Mosher <ben@answerrocket.com>
2014-04-27 18:35:14 -0400

    read_data, get_features

--- run_analysis.R
+++ run_analysis.R
@@ -5,3 +5,19 @@ downloadfiles <- function() {
 	unzip(t)
 	unlink(t)
 }
+
+f <- NULL
+get_features <- function() {
+	load_features <- function() {
+		f <<- read.table("UCI HAR Dataset/features.txt", sep="", row.names=1, stringsAsFactors=FALSE)[,1]
+	}
+	if (is.null(f)) {
+		message("loading features")
+		load_features()
+	}
+	f
+}
+
+read_data <- function(group) {
+	read.table(paste("UCI HAR Dataset/",group,"/X_",group,".txt", sep=""), sep="", col.names=get_features())
+}
